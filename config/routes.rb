f5d1065986cdb49467aeffc21ae1719324abd4c9
Linda Le <yueling1222@gmail.com>
2019-11-20 21:15:32 -0700

    Add updated view and working feature test for Shelter Creation User Story 4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/', to: 'shelters#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
-  post 'shelters', to: 'shelters#create'
+  post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
 
 end
--- spec/features/shelter_creation_spec.rb
+++ spec/features/shelter_creation_spec.rb
@@ -1,28 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "shelters id page", type: :feature do
-  it "can hold a form to create a shelter" do
+RSpec.describe 'shelters id page', type: :feature do
+  it 'can hold a form to create a shelter' do
 
-    visit "/shelters"
-    expect(page).to have_link("Create New Shelter")
-    click_link "Create New Shelter"
+    visit '/shelters'
+
+    expect(page).to have_link('Create New Shelter')
+    click_link 'Create New Shelter'
+
+    expect(current_path).to eq '/shelters/new'
+
+    expect(page).to have_selector('form')
+    expect(page).to have_field('shelter[name]')
+    expect(page).to have_field('shelter[address]')
+    expect(page).to have_field('shelter[city]')
+    expect(page).to have_field('shelter[state]')
+    expect(page).to have_field('shelter[zip_code]')
+
+  end
+
+  it 'can take information from the form and make a new shelter' do
+
     visit '/shelters/new'
 
-    expect(page).to have_field(shelter[name])
-    expect(page).to have_field(shelter[address])
-    expect(page).to have_field(shelter[city])
-    expect(page).to have_field(shelter[state])
-    expect(page).to have_field(shelter[zip_code])
+    fill_in 'shelter[name]',      with: 'Shelter Test'
+    fill_in 'shelter[address]',   with: '123 Larimer Street'
+    fill_in 'shelter[city]',      with: 'Denver'
+    fill_in 'shelter[state]',     with: 'Colorado'
+    fill_in 'shelter[zip_code]',  with: '80001'
 
-    fill_in 'shelter[name]',    with: 'Shelter Test'
-    fill_in 'shelter[address]', with: '123 Larimer Street'
-    fill_in 'shelter[city]',    with: 'Denver'
-    fill_in 'shelter[state]',   with: 'Colorado'
-    fill_in 'shelter[zip_code]',     with: '80001'
+    expect(page).to have_button('Submit')
+    click_button 'Submit'
 
-    expect(page).to have_button("Submit")
-    click_button "Submit"
-    expect(response).to have_http_status(:created)
     assert_equal '/shelters', current_path
   end
 end
