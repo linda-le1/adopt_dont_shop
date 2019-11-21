1ff8d3a5c69d76e7f410d659b46694135bd75bfd
Linda Le <yueling1222@gmail.com>
2019-11-21 16:19:57 -0700

    Add model, spec, and data migration for pets index user story 7:

---
+++ spec/features/pet/pet_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'pets index page', type: :feature do
+  it 'can see all pets' do
+    tofu = Pet.create!(image: '/',
+                       name: 'Tofu',
+                       approximate_age: 4,
+                       sex: 'M',
+                       shelter_name: 'Denver Animal Shelter'
+                       )
+
+    visit '/pets'
+
+    expect(page).to have_content(tofu.name)
+  end
+end
