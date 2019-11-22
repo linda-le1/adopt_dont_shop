d53d9f523ace8bbbf33b79a0a968eb1f9848da8f
Linda Le <yueling1222@gmail.com>
2019-11-21 20:49:16 -0700

    Update pets table to include column for adoption status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121231800) do
+ActiveRecord::Schema.define(version: 20191122034719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20191121231800) do
     t.string "shelter_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_adoptable"
   end
 
   create_table "shelters", force: :cascade do |t|
