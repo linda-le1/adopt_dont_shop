0f7f826fb868d495e5a26a2cf25d53e6a80bc02e
Linda Le <yueling1222@gmail.com>
2019-11-24 23:52:24 -0700

    Add delete links to every shelter on index page with passing tests

--- spec/features/shelter/shelter_index_spec.rb
+++ spec/features/shelter/shelter_index_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe 'shelters index page', type: :feature do
 
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_link('Edit This Shelter', href: "/shelters/#{shelter_1.id}/edit")
+    expect(page).to have_link('Delete This Shelter', href: "/shelters/#{shelter_1.id}")
     expect(page).to have_content(shelter_2.name)
     expect(page).to have_link('Edit This Shelter', href: "/shelters/#{shelter_2.id}/edit")
+    expect(page).to have_link('Delete This Shelter', href: "/shelters/#{shelter_2.id}")
   end
 end
