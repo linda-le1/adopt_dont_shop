c84151d75759c5e727c3918398377712713b7ccf
Linda Le <yueling1222@gmail.com>
2019-11-24 22:46:33 -0700

    Add link to pet show view

--- spec/features/pet/pet_update_spec.rb
+++ spec/features/pet/pet_update_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'as a user', type: :feature do
   it 'can update a pet on the show page'  do
-    
+
     shelter_1 = Shelter.create!(name: 'Denver Animal Shelter',
                                address: '123 Colfax Ave',
                                city: 'Denver',
@@ -18,5 +18,9 @@ RSpec.describe 'as a user', type: :feature do
                        )
 
     visit "pets/#{dog_1.id}"
+
+    click_on 'Edit Pet'
+    assert_equal "/pets/#{dog_1.id}/edit", current_path
+
   end
 end
