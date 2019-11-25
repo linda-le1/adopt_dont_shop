1ef18447eaaee7c6be4b23c7ef9c5a29904d08d8
Linda Le <yueling1222@gmail.com>
2019-11-25 12:21:24 -0700

    Add changes to test spec files

--- spec/features/shelter/shelter_delete_spec.rb
+++ spec/features/shelter/shelter_delete_spec.rb
@@ -1,28 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'As a visitor', type: :feature do
+  describe "After visiting a shelters show page and clicking on delete that shelter" do
+    before :each do
 
-  before :each do
+      @shelter_1 = Shelter.create(name: 'Denver Animal Shelter',
+                                     address: '123 Colfax Ave',
+                                     city: 'Denver',
+                                     state: 'CO',
+                                     zip_code: '80004')
 
-    @shelter_1 = Shelter.create(name: 'Denver Animal Shelter',
-                                   address: '123 Colfax Ave',
-                                   city: 'Denver',
-                                   state: 'CO',
-                                   zip_code: '80004')
+    end
 
-  end
+    it 'can delete an existing shelter' do
 
-  it 'can delete an existing shelter' do
+      visit "/shelters/#{@shelter_1.id}"
+      expect(page).to have_link('All Pets', href: "/pets")
+      expect(page).to have_link('All Shelters', href: "/shelters")
 
-    visit "/shelters/#{@shelter_1.id}"
-    expect(page).to have_link('All Pets', href: "/pets")
-    expect(page).to have_link('All Shelters', href: "/shelters")
+      expect(page).to have_link('Delete This Shelter')
+      click_link 'Delete This Shelter'
 
-    expect(page).to have_link('Delete This Shelter')
-    click_link 'Delete This Shelter'
+      expect(current_path).to eq "/shelters"
 
-    expect(current_path).to eq "/shelters"
-
-    expect(page).to_not have_content(@shelter_1.name)
+      expect(page).to_not have_content(@shelter_1.name)
+    end
   end
 end
--- spec/features/shelter/shelter_edit_spec.rb
+++ spec/features/shelter/shelter_edit_spec.rb
@@ -1,42 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe 'As a visitor', type: :feature do
+  describe "After visiting a shelters show page and clicking on edit that shelter" do
+    before :each do
 
-  before :each do
+      @shelter_1 = Shelter.create(name: 'Denver Animal Shelter',
+                                 address: '123 Colfax Ave',
+                                 city: 'Denver',
+                                 state: 'CO',
+                                 zip_code: '80004')
 
-    @shelter_1 = Shelter.create(name: 'Denver Animal Shelter',
-                               address: '123 Colfax Ave',
-                               city: 'Denver',
-                               state: 'CO',
-                               zip_code: '80004')
+    end
 
-  end
+    xit 'can see prepopulated info on that shelter in the form' do
+    end
 
-  it 'can update shelter information through a form' do
+    it 'can update shelter information through a form' do
 
-    visit "/shelters/#{@shelter_1.id}"
+      visit "/shelters/#{@shelter_1.id}"
 
-    expect(page).to have_link('Update This Shelter')
+      expect(page).to have_link('Update This Shelter')
 
-    click_link 'Update This Shelter'
-    expect(current_path).to eq "/shelters/#{@shelter_1.id}/edit"
+      click_link 'Update This Shelter'
+      expect(current_path).to eq "/shelters/#{@shelter_1.id}/edit"
 
-    expect(page).to have_field('name')
-    expect(page).to have_field('address')
-    expect(page).to have_field('city')
-    expect(page).to have_field('state')
-    expect(page).to have_field('zip_code')
+      expect(page).to have_field('name')
+      expect(page).to have_field('address')
+      expect(page).to have_field('city')
+      expect(page).to have_field('state')
+      expect(page).to have_field('zip_code')
 
-    fill_in 'name',      with: 'Shelter Example 1'
-    fill_in 'address',   with: '987 Colorado Boulevard'
+      fill_in 'name',      with: 'Shelter Example 1'
+      fill_in 'address',   with: '987 Colorado Boulevard'
 
-    expect(page).to have_button('Submit')
+      expect(page).to have_button('Submit')
 
-    click_on('Submit')
-    expect(current_path).to eq "/shelters/#{@shelter_1.id}"
+      click_on('Submit')
+      expect(current_path).to eq "/shelters/#{@shelter_1.id}"
 
-    expect(page).to have_content('Shelter Example 1')
-    expect(page).to have_content('987 Colorado Boulevard')
+      expect(page).to have_content('Shelter Example 1')
+      expect(page).to have_content('987 Colorado Boulevard')
 
+    end
   end
 end
