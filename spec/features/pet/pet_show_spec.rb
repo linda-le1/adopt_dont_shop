162d9e18cccedae8626e5c0c2faf010eb6c6a8f9
Linda Le <yueling1222@gmail.com>
2019-11-24 18:44:31 -0700

    Add passing routes, controller method for show, view, and spec test for user story 9

--- spec/features/pet/pet_show_spec.rb
+++ spec/features/pet/pet_show_spec.rb
@@ -16,9 +16,7 @@ RSpec.describe 'pets show page', type: :feature do
                        sex: 'M',
                        shelter_name: 'Denver Animal Shelter'
                        )
-
-
-    visit '/pets/:id'
+    visit "pets/#{dog_1.id}"
 
     expect(page).to have_content(dog_1.image)
     expect(page).to have_content(dog_1.name)
