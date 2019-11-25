cd1b995829604f9eb1e3aea88e04deec484fc0a1
Linda Le <yueling1222@gmail.com>
2019-11-24 23:48:38 -0700

    Add edit link to every shelter on shelteri ndex page

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,30 @@ shelter_1 = Shelter.create!(name: 'Denver Animal Shelter',
                            state: 'CO',
                            zip_code: '80004')
 
-dog_1 = shelter_1.pets.create!(image: '/',
-                   name: 'Tofu',
-                   description: 'I am a neutered male, white Terrier Mix who loves to play fetch.',
-                   approximate_age: 4,
-                   sex: 'M',
-                   shelter_name: 'Denver Animal Shelter'
-                   )
+shelter_2 = Shelter.create!(name: 'Colorado Shiba Inu Rescue',
+                            address: '777 Colorado Blvd',
+                            city: 'Denver',
+                            state: 'CO',
+                            zip_code: '80012')
+
+shelter_1.pets.create!(image: '/',
+                       name: 'Tofu',
+                       description: 'I am a neutered male, white Terrier Mix who loves to play fetch.',
+                       approximate_age: 4,
+                       sex: 'M',
+                       shelter_name: 'Denver Animal Shelter'
+                       )
+shelter_1.pets.create!(image: '/',
+                        name: 'Kumo',
+                        description: 'I am a neutered male, black Shiba Inu who loves to go on hikes.',
+                        approximate_age: 2,
+                        sex: 'M',
+                        shelter_name: 'Denver Animal Shelter'
+                      )
+shelter_2.pets.create!(image: '/',
+                        name: 'Kiku',
+                        description: 'I am a spayed female, cream Shiba Inu who loves to play fetch.',
+                        approximate_age: 5,
+                        sex: 'F',
+                        shelter_name: 'Colorado Shiba Inu Rescue'
+                        )
--- spec/features/shelter/shelter_index_spec.rb
+++ spec/features/shelter/shelter_index_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe 'shelters index page', type: :feature do
   it 'can see all shelters' do
     shelter_1 = Shelter.create!(name: 'Denver Animal Shelter')
+    shelter_2 = Shelter.create!(name: 'Colorado Shiba Inu Rescue')
 
     visit '/shelters'
 
     expect(page).to have_content(shelter_1.name)
+    expect(page).to have_link('Edit This Shelter', href: "/shelters/#{shelter_1.id}/edit")
+    expect(page).to have_content(shelter_2.name)
+    expect(page).to have_link('Edit This Shelter', href: "/shelters/#{shelter_2.id}/edit")
   end
 end
