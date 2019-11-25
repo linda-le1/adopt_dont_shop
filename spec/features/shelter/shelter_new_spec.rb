39fdcf254f29295372a4793484a3fc87c3caef36
Linda Le <yueling1222@gmail.com>
2019-11-25 12:31:53 -0700

    Add changes to shelter new spec file

--- spec/features/shelter/shelter_new_spec.rb
+++ spec/features/shelter/shelter_new_spec.rb
@@ -1,22 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'As a visitor', type: :feature do
-  it 'can create a shelter' do
+RSpec.describe 'Shelter new page', type: :feature do
+  describe "As a visitor" do
 
-    visit '/shelters'
+    it 'can create a shelter' do
 
-    expect(page).to have_link('Create New Shelter')
-    click_link 'Create New Shelter'
+      visit '/shelters'
+      click_on 'Create New Shelter'
 
-    expect(current_path).to eq '/shelters/new'
+      name = 'Shelter Test'
+      address = '123 Larimer Street'
+      city = 'Denver'
+      state = 'CO'
+      zip_code = '80001'
 
-    fill_in 'name',      with: 'Shelter Test'
-    fill_in 'address',   with: '123 Larimer Street'
-    fill_in 'city',      with: 'Denver'
-    fill_in 'state',     with: 'Colorado'
-    fill_in 'zip_code',  with: '80001'
-    click_button 'Submit'
+      fill_in :name, with: name
+      fill_in :address, with: address
+      fill_in :city, with: city
+      fill_in :state, with: state
+      fill_in :zip_code, with: zip_code
+      click_on 'Submit'
 
-    assert_equal '/shelters', current_path
+      new_shelter = Shelter.last
+      assert_equal '/shelters', current_path
+
+      expect(page).to have_content(name)
+      expect(new_shelter.name).to eq(name)
+      expect(new_shelter.address).to eq(address)
+      expect(new_shelter.city).to eq(city)
+      expect(new_shelter.state).to eq(state)
+      expect(new_shelter.zip_code).to eq(zip_code)
+
+    end
   end
 end
