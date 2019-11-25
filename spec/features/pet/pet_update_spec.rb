d0a9288b42bccc30e6f711fdfcefd25b8dbb4431
Linda Le <yueling1222@gmail.com>
2019-11-24 22:34:54 -0700

    Seed database and add pet update spec file

---
+++ spec/features/pet/pet_update_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'as a user', type: :feature do
+  it 'can update a pet on the show page'  do
+    
+    shelter_1 = Shelter.create!(name: 'Denver Animal Shelter',
+                               address: '123 Colfax Ave',
+                               city: 'Denver',
+                               state: 'CO',
+                               zip_code: '80004')
+
+    dog_1 = shelter_1.pets.create!(image: '/',
+                       name: 'Tofu',
+                       description: 'I am a neutered male, white Terrier Mix who loves to play fetch.',
+                       approximate_age: 4,
+                       sex: 'M',
+                       shelter_name: 'Denver Animal Shelter'
+                       )
+
+    visit "pets/#{dog_1.id}"
+  end
+end
