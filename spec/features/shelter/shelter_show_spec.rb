04b4978b46d289a891ac688df9d20262f5ca25c8
Linda Le <yueling1222@gmail.com>
2019-11-25 11:33:30 -0700

    Add changes to shelter edit and show spex files

--- spec/features/shelter/shelter_show_spec.rb
+++ spec/features/shelter/shelter_show_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
+RSpec.describe 'shelters show page', type: :feature do
 
-RSpec.describe 'shelters id pages', type: :feature do
-  it 'can see individual shelter information' do
+  before :each do
 
-    shelter_1 = Shelter.create!(name: 'Denver Animal Shelter',
+    @shelter_1 = Shelter.create(name: 'Denver Animal Shelter',
                                address: '123 Colfax Ave',
                                city: 'Denver',
                                state: 'CO',
                                zip_code: '80004')
 
-    visit "/shelters/#{shelter_1.id}"
+  end
+
+  it 'can hold links to shelter and pet index pages' do
+
+    visit "/shelters/#{@shelter_1.id}"
+
     expect(page).to have_link('All Pets', href: "/pets")
     expect(page).to have_link('All Shelters', href: "/shelters")
-    expect(page).to have_link('Adoptable Pets', href:"/shelters/#{shelter_1.id}/pets")
 
-    expect(page).to have_content(shelter_1.name)
-    expect(page).to have_content(shelter_1.address)
-    expect(page).to have_content(shelter_1.city)
-    expect(page).to have_content(shelter_1.state)
-    expect(page).to have_content(shelter_1.zip_code)
+  end
+
+  it 'can see individual shelter information including adoptable pets' do
+
+    visit "/shelters/#{@shelter_1.id}"
+
+    expect(page).to have_link('Adoptable Pets', href:"/shelters/#{@shelter_1.id}/pets")
+
+    expect(page).to have_content(@shelter_1.name)
+    expect(page).to have_content(@shelter_1.address)
+    expect(page).to have_content(@shelter_1.city)
+    expect(page).to have_content(@shelter_1.state)
+    expect(page).to have_content(@shelter_1.zip_code)
+
   end
 end
