41a621dc2bacc907b4c00794a43069ec744e0cee
Linda Le <yueling1222@gmail.com>
2019-11-26 16:21:41 -0700

    Add pet model test and validations to model file

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to:shelter
+  validates_presence_of :image_url, :name
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :sex
+  validates_presence_of :approximate_age
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,9 @@
 class Shelter < ApplicationRecord
-  has_many:pets 
+  has_many :pets
+  validates_associated :pets
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip_code
 end
