85ea8d3596375fa588513382b56582af7bf7463b
Linda Le <yueling1222@gmail.com>
2019-11-25 20:00:47 -0700

    Add passing image test to show spec file and add image tag to pet show view

--- spec/features/pet/pet_show_spec.rb
+++ spec/features/pet/pet_show_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe 'Pets show page', type: :feature do
                                  state: 'CO',
                                  zip_code: '80004')
 
-      @dog_1 = @shelter_1.pets.create!(image: '/',
+      @dog_1 = @shelter_1.pets.create!(image_url: '/',
                          name: 'Tofu',
                          description: 'I am a neutered male, white Terrier Mix who loves to play fetch.',
                          approximate_age: 4,
-                         sex: 'M',
-                         shelter_name: 'Denver Animal Shelter'
+                         sex: 'Male',
                          )
       end
 
@@ -41,7 +40,7 @@ RSpec.describe 'Pets show page', type: :feature do
 
       visit "pets/#{@dog_1.id}"
 
-      expect(page).to have_content(@dog_1.image)
+      expect(page).to have_xpath("//img[@src='#{@dog_1.image_url}']")
       expect(page).to have_content(@dog_1.name)
       expect(page).to have_content(@dog_1.description)
       expect(page).to have_content(@dog_1.approximate_age)
